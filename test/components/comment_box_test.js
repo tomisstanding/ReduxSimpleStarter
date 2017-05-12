dd24333b94a819ae46a903023537374998077582
Thomas Standing <tomcoap@gmail.com>
2017-05-11 22:27:23 -0400

    add tests for submitting comments

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -2,18 +2,35 @@ import { renderComponent, expect } from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
-  it('has the correct class', () => {
+  let component;
+
+  beforeEach(() => {
     const component = renderComponent(CommentBox);
+  });
+
+  it('has the correct class', () => {
     expect(component).to.have.class('comment-box');
   });
 
   it('has a text area', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('textarea')).to.exist;
   });
 
   it('has a button', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('button')).to.exist;
   });
+
+  describe('entering some text', () => {
+    beforeEach(() => {
+      component.find('textarea').simulate('change', 'new comment');
+    });
+
+    it('shows text that is entered', () => {
+
+    });
+
+    it('when submitted, clears the input', () => {
+
+    });
+  })
 });

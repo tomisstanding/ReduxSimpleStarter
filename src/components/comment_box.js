3eab18105ba76832d4823a7b9bdef9692435bf24
Thomas Standing <tomcoap@gmail.com>
2017-05-12 09:51:35 -0400

    add initial state for comment box component

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -2,24 +2,30 @@ import React, { Component } from 'react';
 
 
 export default class CommentBox extends Component {
-  constuctor(props) {
+  constructor(props) {
     super(props);
 
-    this.state = { comment: ''};
+    this.state = { comment: '' };
   }
 
   handleChange(event) {
     this.setState({ comment: event.target.value });
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.setState({ comment: '' });
+  }
+
   render() {
     return (
-      <div className="comment-box">
+      <form onSubmit={this.handleSubmit.bind(this)} className="comment-box">
         <textarea
           value={this.state.comment}
           onChange={this.handleChange.bind(this)} />
-        <button>Submit Comment</button>
-      </div>
+        <button action="submit">Submit Comment</button>
+      </form>
     );
   }
 }
--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -31,7 +31,8 @@ describe('CommentBox', () => {
     });
 
     it('when submitted, clears the input', () => {
-      expect()
+      component.simulate('submit');
+      expect(component.find('textarea')).to.have.value('');
     });
   })
 });
